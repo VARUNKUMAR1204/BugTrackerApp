6550d9673ec6ce7a3e8c0f25cc63d5499bfac26b
VARUNKUMAR1204 <varunmanchikanti01@gmail.com>
2025-08-18 13:15:58 +0530

    Updated AuthController and switched to updat mode in application.properties

--- backend/src/main/java/com/example/bugtracker/controller/AuthController.java
+++ backend/src/main/java/com/example/bugtracker/controller/AuthController.java
@@ -9,11 +9,13 @@ import com.example.bugtracker.service.AuthService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "*")
 public class AuthController {
 
     @Autowired
